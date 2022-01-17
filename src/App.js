1d38ec28a8cb02f2655783866717c9a4d9f6c81c
Agustina Marijanovic <Agustina.Marijanovic@endava.com>
2022-01-17 16:40:09 -0300

    config: add tailwind, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Card from "./Card";
-
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <Card/>
+    <div>
+      <Card />
     </div>
   );
 }
